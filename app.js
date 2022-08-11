ab267719fe6090fbddd7d5dc119430cc2fda6760
TheBigH50 <HFangmeier@outlook.com>
2022-08-11 14:04:19 -0500

    exe3

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const firstName = "Harrison"
-let lastName = "Fangmeier"
-var age = 34
+const firstName = "Harrison";
+let lastName = "Fangmeier";
+var age = 34;
 
-
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = false;
+let selectedSize = "M";
